b29902fdadc492afaa701c6d97c646ef5ff96c41
hishamrehab <hishamrehab30@gmail.com>
2026-01-04 23:25:46 +0200

    Action and Action Creator

---
+++ 1-  Redux/script.js
@@ -0,0 +1,16 @@
+const ORDER_PIZZA = "ORDER_PIZZA"
+
+// Action 
+// const action = {
+//     type : ORDER_PIZZA,
+//     shop_name : "Pizza Shop"
+// }
+
+
+// Action Creator
+function orderPizza() {
+    return {
+    type : ORDER_PIZZA,
+    shop_name : "Pizza Shop"
+}
+}
