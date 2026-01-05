64a9ad308639b38a2359c8df266fc82a82e6c5fe
hishamrehab <hishamrehab30@gmail.com>
2026-01-05 21:09:22 +0200

    implement Actions and Action Creators

--- 2 -  React Redux/react-redux/src/components/PizzaBox.jsx
+++ 2 -  React Redux/react-redux/src/components/PizzaBox.jsx
@@ -2,7 +2,7 @@ const PizzaBox = () => {
   return (
     <div className="container">
       <h2 className="text">
-    Number of Pizza Base available - 100
+        Number of Pizza Base available - 100
       </h2>
       <button className="btn">
         Order Pizza
